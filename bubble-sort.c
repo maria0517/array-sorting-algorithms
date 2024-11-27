dffdd736ec6db9829e95e2acb765bcedc10f08b6
Maria Oprea <maria.oprea.mar@gmail.com>
2024-11-27 16:37:22 +0200

    Add Bubble Sort Skeleton

---
+++ bubble-sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	#include <stdio.h>
+ 
+static void sort(void)
+{
+    // TODO: add bubble sort algorithm here
+}
+ 
+int main()
+{
+    return 0;
+}
+
+	return 0;
+}
